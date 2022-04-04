69b25af8d8de7695591f3d310214954030fb8cf4
SSJDeathSpawn <adikesavan28@gmail.com>
2022-04-04 13:55:20 +0530

    README.md and command line arguments

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
+# QOI Encoder
+
 To run the program,
 Just run main.py (with python3)
 
 Enter the path to the image and the final qoi format image (w/ or w/o extension)
+
+Linux:
+`$ python3 main.py [png] [qoi]`
+
+Windows:
+You can probably just double click main.py if you have Python installed.
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from ast import arg
 import numpy as np
 import pickle
+import sys
 
 from PIL import Image
 from file import QOIFile
@@ -19,11 +21,11 @@ def has_transparency(img):
     return False
 
 if __name__=="__main__":
-    path = input("Enter path to image (png):")
+    path = sys.argv[1] if sys.argv and len(sys.argv) > 1 else input("Enter path to image (png):")
     image = Image.open(path + (".png" if path[-4:] != ".png" else ""))
     image = image.convert('RGBA' if (has_transparency(image)) else 'RGB')
 
-    write_path = input("Enter path of final image (qoi):")
+    write_path = sys.argv[2] if sys.argv and len(sys.argv) > 2 else input("Enter path of final image (qoi):")
     write_path += (".qoi" if write_path[-4:] != ".qoi" else "")
 
     np_img = np.asarray(image)
